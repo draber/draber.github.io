b9e95f0f915f18885ee8c63a107920ab8625733e
Dieter Raber <dieter@taotesting.com>
2021-01-01 00:04:38 +0100

    fixed bug in url

--- src/templates/bookmarklet/main.js
+++ src/templates/bookmarklet/main.js
@@ -1,4 +1,4 @@
 const script = document.createElement('script');
 script.async = true;
-script.src = 'https://spelling-bee-assistant.app/img/spelling-bee-assistant.min.js';
+script.src = 'https://spelling-bee-assistant.app/assets/js/spelling-bee-assistant.min.js';
 document.body.append(script);
