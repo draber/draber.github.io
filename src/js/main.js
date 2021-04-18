eb9872894cf07a04b5a4e6214551e8ef84d273c9
Dieter Raber <dieter@taotesting.com>
2021-04-18 20:59:47 +0200

    Bug fixes related to new Spelling Bee Layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 import el from './modules/element.js';
 import App from './modules/app.js';
 import data from './modules/data.js';
-import plugins from './modules/importer.js';import {
+import plugins from './modules/importer.js';
+import {
     prefix
 } from './modules/string.js';
 
 const app = new App(el.$('#pz-game-root'));
-app.getResults()
-    .then(foundTerms => {
-        data.init(app, foundTerms);
-        app.registerPlugins(plugins);
-        app.trigger(prefix('wordsUpdated'));
-    });
 
+app.launch().then(() => {
+    app.getResults()
+        .then(foundTerms => {
+            data.init(app, foundTerms);
+            app.registerPlugins(plugins);
+            app.trigger(prefix('wordsUpdated'));
+        });
+})
