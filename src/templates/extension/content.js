58cb2cd3cbd1b58da7fdce5b8f962910d5dec4ba
draber <me@dieterraber.net>
2021-07-12 07:48:00 +0200

    new api in readme

--- src/templates/extension/content.js
+++ src/templates/extension/content.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
 	const el = document.createElement('script');
 	el.async = true;
-	el.src = chrome.extension.getURL('{{sbaFileName}}');
+	el.src = chrome.runtime.getURL('{{sbaFileName}}');
 	el.id = '{{name}}';
 	document.body.append(el);
 })
