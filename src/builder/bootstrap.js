34a8d4a711c2447366d1f4cda890fe4f064768ef
draber <me@dieterraber.net>
2026-02-06 09:35:52 +0100

    fixed formatting

--- src/builder/bootstrap.js
+++ src/builder/bootstrap.js
@@ -3,7 +3,8 @@ import config from "../config/config.json" with { type: "json" };
 import pkg from "../../package.json" with { type: "json" };
 
 const args = minimist(process.argv.slice(2));
-const env = args["config-env"] && args["config-env"] === "prod" ? "prod" : "dev";
+const env =
+    args["config-env"] && args["config-env"] === "prod" ? "prod" : "dev";
 
 const outputFile = env === "dev" ? `${pkg.name}.js` : `${pkg.name}.min.js`;
 
